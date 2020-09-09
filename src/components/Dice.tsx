cf4aec0db445efac9b40e7dd19f1f019ffe4c610
grakify90 <ninavanes1990@hotmail.com>
2020-09-09 17:59:17 +0200

    made the dice work

--- src/components/Dice.tsx
+++ src/components/Dice.tsx
@@ -1,9 +1,23 @@
 import React from "react";
+import { Button } from "@material-ui/core";
 
 type DiceProps = {
-  dices: string;
+  onedice: { dice: string; locked: boolean; location: number };
+  callback: (place: number) => void;
 };
 
 export const Dice: React.FC<DiceProps> = (props) => {
-  return <img src={props.dices} style={{ width: "100px" }} alt="dice" />;
+  return (
+    <div style={{ width: "100px" }}>
+      <img src={props.onedice.dice} style={{ width: "100%" }} alt="dice" />
+
+      <Button
+        onClick={props.callback.bind(null, props.onedice.location)}
+        variant="contained"
+        color="primary"
+      >
+        {props.onedice.locked ? "Release" : "Keep"}
+      </Button>
+    </div>
+  );
 };
